87e0e9e5a38ae1bb183c75c6e80ee23cf99ca017
hamidreza <hamidreza004@gmail.com>
2018-10-05 17:12:10 +0330

    change colors

--- static/main/js/creative.js
+++ static/main/js/creative.js
@@ -1,196 +1,200 @@
-(function($) {
-     "use strict"; // Start of use strict
+(function ($) {
+    "use strict"; // Start of use strict
 
-	 // Smooth scrolling using jQuery easing
-	     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-				         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-						             var target = $(this.hash);
-									             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-												             if (target.length) {
-															                 $('html, body').animate({
-																					                     scrollTop: (target.offset().top - 57)
-																										                 }, 1000, "easeInOutExpo");
-																			                 return false;
-																							             }
-																										         }
-																												     });
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: (target.offset().top - 57)
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
 
-		 // Closes responsive menu when a scroll trigger link is clicked
-		     $('.js-scroll-trigger').click(function() {
-					         $('.navbar-collapse').collapse('hide');
-							     });
+    // Closes responsive menu when a scroll trigger link is clicked
+    $('.js-scroll-trigger').click(function () {
+        $('.navbar-collapse').collapse('hide');
+    });
 
-			 // Activate scrollspy to add active class to navbar items on scroll
-			     $('body').scrollspy({
-						         target: '#mainNav',
-								         offset: 150
-										     });
+    // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+        target: '#mainNav',
+        offset: 150
+    });
 
-				 // Collapse Navbar
-				     var navbarCollapse = function() {
-						         if ($("#mainNav").offset().top > 100) {
-									             $("#mainNav").addClass("navbar-shrink");
-												         } else {
-															             $("#mainNav").removeClass("navbar-shrink");
-																		         }
-								     };
-					 // Collapse now if page is not at top
-					     navbarCollapse();
-						 // Collapse the navbar when page is scrolled
-						     $(window).scroll(navbarCollapse);
+    // Collapse Navbar
+    var navbarCollapse = function () {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-shrink");
+        } else {
+            $("#mainNav").removeClass("navbar-shrink");
+        }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
 
-							 // Scroll reveal calls
-							     window.sr = ScrollReveal();
-								     sr.reveal('.sr-icons', {
-											         duration: 600,
-													         scale: 0.3,
-															         distance: '0px'
-																	     }, 200);
-									     sr.reveal('.sr-button', {
-												         duration: 1000,
-														         delay: 200
-																     });
-										     sr.reveal('.sr-contact', {
-													         duration: 600,
-															         scale: 0.3,
-																	         distance: '0px'
-																			     }, 300);
+    // Scroll reveal calls
+    window.sr = ScrollReveal();
+    sr.reveal('.sr-icons', {
+        duration: 600,
+        scale: 0.3,
+        distance: '0px'
+    }, 200);
+    sr.reveal('.sr-button', {
+        duration: 1000,
+        delay: 200
+    });
+    sr.reveal('.sr-contact', {
+        duration: 600,
+        scale: 0.3,
+        distance: '0px'
+    }, 300);
 
-											 // Magnific popup calls
-											     $('.popup-gallery').magnificPopup({
-														         delegate: 'a',
-																         type: 'image',
-																		         tLoading: 'Loading image #%curr%...',
-																				         mainClass: 'mfp-img-mobile',
-																						         gallery: {
-																								             enabled: true,
-																											             navigateByImgClick: true,
-																														             preload: [0, 1]
-																																	         },
-																																			         image: {
-																																					             tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-																																								         }
-																																										     });
-
-
-												     function getCountdown(){
-
-														 // find the amount of "seconds" between now and target
-														         var seconds_left = (target_time - new Date().getTime())/1000;
-																         days = pad( parseInt(seconds_left / 86400) );
-																		         seconds_left = seconds_left % 86400;
-
-																				         hours = pad( parseInt(seconds_left / 3600) );
-																						         seconds_left = seconds_left % 3600;
-
-																								         minutes = pad( parseInt(seconds_left / 60) );
-																										         seconds = pad( parseInt( seconds_left % 60 ) );
-
-																												 // format countdown string + set tag value
-																												         countdown.innerHTML = "<span>" + days + "</span><span>" + hours + "</span><span>" + minutes + "</span><span>" + seconds + "</span>";
-																														     }
-
-													     function pad(n) {
-															         var a={0:'۰',1:'۱',2:'۲',3:'۳',4:'۴',5:'۵',6:'۶',7:'۷',8:'۸',9:'۹'};
-																	         return a[parseInt(n/10)] + a[n%10];
-																			     }
-														     var target_time = new Date().getTime() + 36000000;
-															     var days, hours, minutes, seconds; // variables for time units
-
-																     var countdown = document.getElementById("tiles"); // get tag element
-
-																	     getCountdown();
-
-																		     setInterval(function () { getCountdown(); }, 1000);
-
-																			     var TxtType = function(el, toRotate, period) {
-																					         this.toRotate = toRotate;
-																							         this.el = el;
-																									         this.loopNum = 0;
-																											         this.period = parseInt(period, 10) || 2000;
-																													         this.txt = '';
-																															         this.tick();
-																																	         this.isDeleting = false;
-																																			     };
-
-																				     TxtType.prototype.tick = function() {
-																						         var i = this.loopNum % this.toRotate.length;
-																								         var fullTxt = this.toRotate[i];
-
-																										         if (this.isDeleting) {
-																													             this.txt = fullTxt.substring(0, this.txt.length - 1);
-																																         } else {
-																																			             this.txt = fullTxt.substring(0, this.txt.length + 1);
-																																						         }
-
-																												         this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-																														         var that = this;
-																																         var delta = 200 - Math.random() * 100;
-
-																																		         if (this.isDeleting) { delta /= 2; }
-
-																																				         if (!this.isDeleting && this.txt === fullTxt) {
-																																							             delta = this.period;
-																																										             this.isDeleting = true;
-																																													         } else if (this.isDeleting && this.txt === '') {
-																																																             this.isDeleting = false;
-																																																			             this.loopNum++;
-																																																						             delta = 500;
-																																																									         }
-
-																																						         setTimeout(function() {
-																																										             that.tick();
-																																													         }, delta);
-																																								     };
-
-																					     window.onload = function() {
-																							         var elements = document.getElementsByClassName('typewrite');
-																									         for (var i=0; i<elements.length; i++) {
-																												             var toRotate = elements[i].getAttribute('data-type');
-																															             var period = elements[i].getAttribute('data-period');
-																																		             if (toRotate) {
-																																						                 new TxtType(elements[i], JSON.parse(toRotate), period);
-																																										             }
-																																					         }
-																											         // INJECT CSS
-																											         var css = document.createElement("style");
-																													         css.type = "text/css";
-																															         css.innerHTML = ".typewrite > .wrap { border-left: 0.08em solid #fff}";
-																																	         document.body.appendChild(css);
-																																			     };
-																						     var options = {
-																								         useEasing: true,
-																													        useGrouping: true,
-																															        separator: ',',
-																																	        decimal: '.',
-																																			    };
-
-																							     $('.counter').each(function() {
-																										         var $this = $(this),
-																												             countTo = $this.attr('data-count');
-
-																															         $({ countNum: $this.text()}).animate({
-																																			                 countNum: countTo
-																																							             },
-
-																																										             {
-
-																																													                 duration: 800,
-																																																	                 easing:'linear',
-																																																					                 step: function() {
-																																																									                     $this.text(Math.floor(this.countNum));
-																																																														                 },
-																																																																		                 complete: function() {
-																																																																						                     $this.text(this.countNum);
-																																																																											                     //alert('finished');
-																																																																											                 }
-
-																																																																															             });
+    // Magnific popup calls
+    $('.popup-gallery').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1]
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+        }
+    });
 
 
+    function getCountdown() {
 
-																																	     });
+        // find the amount of "seconds" between now and target
+        var seconds_left = (target_time - new Date().getTime()) / 1000;
+        days = pad(parseInt(seconds_left / 86400));
+        seconds_left = seconds_left % 86400;
+
+        hours = pad(parseInt(seconds_left / 3600));
+        seconds_left = seconds_left % 3600;
+
+        minutes = pad(parseInt(seconds_left / 60));
+        seconds = pad(parseInt(seconds_left % 60));
+
+        // format countdown string + set tag value
+        countdown.innerHTML = "<span>" + days + "</span><span>" + hours + "</span><span>" + minutes + "</span><span>" + seconds + "</span>";
+    }
+
+    function pad(n) {
+        var a = {0: '۰', 1: '۱', 2: '۲', 3: '۳', 4: '۴', 5: '۵', 6: '۶', 7: '۷', 8: '۸', 9: '۹'};
+        return a[parseInt(n / 10)] + a[n % 10];
+    }
+
+    var target_time = new Date().getTime() + 36000000;
+    var days, hours, minutes, seconds; // variables for time units
+
+    var countdown = document.getElementById("tiles"); // get tag element
+
+    getCountdown();
+
+    setInterval(function () {
+        getCountdown();
+    }, 1000);
+
+    var TxtType = function (el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function () {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+            this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+            this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) {
+            delta /= 2;
+        }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+            delta = this.period;
+            this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+            this.isDeleting = false;
+            this.loopNum++;
+            delta = 500;
+        }
+
+        setTimeout(function () {
+            that.tick();
+        }, delta);
+    };
+
+    window.onload = function () {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i = 0; i < elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+                new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-left: 0.08em solid #fff}";
+        document.body.appendChild(css);
+    };
+    var options = {
+        useEasing: true,
+        useGrouping: true,
+        separator: ',',
+        decimal: '.',
+    };
+
+    $('.counter').each(function () {
+        var $this = $(this),
+            countTo = $this.attr('data-count');
+
+        $({countNum: $this.text()}).animate({
+                countNum: countTo
+            },
+
+            {
+
+                duration: 800,
+                easing: 'linear',
+                step: function () {
+                    $this.text(Math.floor(this.countNum));
+                },
+                complete: function () {
+                    $this.text(this.countNum);
+                    //alert('finished');
+                }
+
+            });
+
+
+    });
 
 
 })(jQuery); // End of use strict
